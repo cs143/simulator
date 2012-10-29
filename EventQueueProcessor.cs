7a48e948c5a65907316c69328e05a4f7007f54e0
Michael Hirshleifer <111mth@caltech.edu>
2012-10-29 06:10:13 +0000

    Implementation of EventQueueProcessor using SortedList and functions

--- EventQueueProcessor.cs
+++ EventQueueProcessor.cs
@@ -1,53 +1,45 @@
 using System.Collections.Generic;
+using System.Linq;
 
 public class Simulator
 {
+	private static EventQueueProcessor eqp = new EventQueueProcessor();
 	static void Main()
 	{
-		var eqp = new EventQueueProcessor();
 		eqp.Add(0.0, StupidEvent(0));
 		eqp.Execute();
 	}
 	
-	private Event StupidEvent(int x)
+	private static Event StupidEvent(int x)
 	{
 		return () => {
-			SimulatorLog.add("Hello, world!" + x);
+			//SimulatorLog.add("Hello, world!" + x);
 			System.Console.WriteLine("Hello, world!" + x);
-			return new Dictionary(){{x + 1, StupidEvent(x + 1)}};
+			eqp.Add(eqp.current_time + 1.0, StupidEvent(x + 1));
 		};
 	}
 }
 
 public class EventQueueProcessor
 {
-	public double current_time { public get; protected set; }
-	private var queue = new SortedDictionary<Double, Event>();
+	public double current_time { get; protected set; }
+	private SortedList<double, Event> queue = new SortedList<double, Event>();
 	public void Add(double time, Event evt)
 	{
-		queue.add(time, evt);
+		queue.Add(time, evt);
 	}
 	/** Runs the events in the event queue, including new events created, until the queue is empty. */
 	public void Execute()
 	{
 		while(queue.Count > 0)
 		{
-			// @@@@pseudocode
-			var next = queue.First();
-			double current_time = next.Key;
+			var next = queue.First<KeyValuePair<double, Event>>();
+			queue.RemoveAt(0);
+			current_time = next.Key;
 			Event next_event = next.Value;
-			IDictionary<double, Event> new_evts = next_event();
-			// Convert times in the future to absolute times.
-			queue.AddAll(mapAt(0, current_time + _, next_event()));
+			next_event();
 		}
 	}
 }
 
-/**
-* Function type. When executed, an event returns a dictionary of events that should be executed in the future.
-* (Time in the future)
-*/
-public delegate void /*IDictionary<double, Event>*/ Event();
-
-
-
+public delegate void Event();
