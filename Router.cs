86e516cd2333f66b3ead8d99880a91b649232170
Michael Hirshleifer <111mth@caltech.edu>
2012-11-18 07:48:45 +0000

    Implement Dijkstra, finding shortest-path tree.
    Currently the calculation is based on the static costs for each link.
    Routers access global state to learn about the network; no information is exchanged between simulated routers.

--- Router.cs
+++ Router.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+// TODO replace with real node class
+using Node = simulator.Host;
+
 namespace simulator
 {
 
@@ -16,13 +19,54 @@ public class Router
     }
     
     /// <summary>
-    /// Event that router is notified that a packet has been received,
-    /// and is now waiting in a link's buffer.
+    /// Event that router @@@@@@
     /// </summary>
-    public Event NotifyReceivedPacket(Packet packet) {
+    public Event ReceivePacket(Packet packet) {
         return () => {
             // TODO
         };
+    }
+    
+    
+    /// <summary>
+    /// Calculates the shortest path to every other node in the network.
+    /// The implementation uses Dijkstra's algorithm, for use in a link-state routing protocol.
+    /// </summary>
+    private IDictionary<Node, Node?> CalculateShortestPaths() {
+        var nodes = Simulator.Hosts.Values;	// TODO all nodes rather than hosts
+        /// Best known distance (sum of costs) from this to each node
+        IDictionary<Node, double> dist = nodes.ToDictionary(node => node, _ => Double.PositiveInfinity);
+        /// Predecessor of each node in shortest-paths tree
+        IDictionary<Node, Node?> previous = nodes.ToDictionary(node => node, _ => null);
+        
+        dist[this] = 0;
+        /// Nodes that have yet to be processed
+        ISet<Node> queue = new HashSet<Node>(nodes); // TODO Priority queue would be faster; replace if needed
+        
+        while(queue.Count > 0) {
+            Node u = queue.Min(node => dist[node]);
+            queue.Remove(u);
+            if(dist[nodes] == Double.PositiveInfinity)
+                break; // remaining nodes are inaccessible
+            
+            foreach(Node v /*out-neighbor of u*/in queue
+                .Where(v => Simulator.LinksBySrcDest.ContainsKey(Tuple.Create(u, v))))
+            {
+                Link uv = Simulator.LinksBySrcDest[Tuple.Create(u, v)];
+                double new_dist = dist[u] + uv.cost;
+                if(new_dist < dist[v]) {
+                    dist[v] = new_dist;
+                    previous[v] = u;
+                    // TODO for priority queue, run queue.DecreaseKey(new_dist, v)
+                }
+            }
+        }
+        
+        return previous;
+    }
+    
+    private void RecalculateRoutingTable() {
+        // TODO
     }
 }
 
