6fabeccf8a37cec5959114aa1330b6405ff8155e
Michael Hirshleifer <111mth@caltech.edu>
2012-11-18 04:06:58 +0000

    Convert Router; scratch implementation

--- Router.cs
+++ Router.cs
@@ -5,12 +5,25 @@ using System.Text;
 
 namespace simulator
 {
-    public class Router
+
+public class Router
+{
+    string Name;
+    private readonly IDictionary<Host, Router> routing_table = new Dictionary<Host, Router>();
+    public Router(string name)
     {
-        string Name;
-        public Router(string name)
-        {
-            this.Name = name;
-        }
+        this.Name = name;
     }
+    
+    /// <summary>
+    /// Event that router is notified that a packet has been received,
+    /// and is now waiting in a link's buffer.
+    /// </summary>
+    public Event NotifyReceivedPacket(Packet packet) {
+        return () => {
+            // TODO
+        };
+    }
+}
+
 }
