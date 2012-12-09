ffe3c02572a8aa167899f5ae20dd9a3367141c29
Michael Hirshleifer <111mth@caltech.edu>
2012-12-09 02:48:37 +0000

    Add forwarding for link-state packets to Node

--- Node.cs
+++ Node.cs
@@ -13,7 +13,24 @@ public abstract class Node
     /// <returns>
     /// Event that occurs when this Node receives a packet.
     /// </returns>
-    public abstract Event ReceivePacket(Packet packet);
+    /// <remarks>
+    /// The event processes link-state advertisement packets (this processing should be common to all nodes on the network).
+    /// When an ordinary packet is received,
+    /// the event (synchronously) calls the ProcessReceivedPacket abstract method,
+    /// which Hosts and Routers override to implement their specific behavior.
+    /// </remarks>
+    public Event ReceivePacket(Packet packet) {
+        return () => {
+            if(packet.type@@@)
+        }
+    }
+    /// <summary>
+    /// Processes the received packet.
+    /// </summary>
+    /// <param name='packet'>
+    /// Packet.
+    /// </param>
+    public abstract void ProcessReceivedPacket(Packet packet);
     
     protected readonly EventQueueProcessor eqp;
     /// <summary>
